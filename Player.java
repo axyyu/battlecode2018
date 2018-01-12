e2dc3a43fa71f6a1a44dc46b80afc48620cd076b
Andrew Wang <andrewxiaoyu0@gmail.com>
2018-01-12 13:40:48 -0500

    Added factory

--- Player.java
+++ Player.java
@@ -1,33 +1,25 @@
 // import the API.
 // See xxx for the javadocs.
 import bc.*;
+import java.util.*;
 
 public class Player {
-
-    // Maps
-    private static PlanetMap earthMap;
-    private static PlanetMap marsMap;
-
-    // Unit
-    private static VecUnit units;
-
-    // Economy
-    private static long karbonite;
 
     public static void main(String[] args) {
         // Connect to the manager, starting the game
         GameController gc = new GameController();
 
         // Map setup
-        earthMap = gc.starting_map(Planet.Earth);
-        marsMap = gc.starting_map(Planet.Mars);
+        // earthMap = gc.starting_map(Planet.Earth);
+        // marsMap = gc.starting_map(Planet.Mars);
 
         // Initial Setup
+        /*
         while (true) {
             units = gc.myUnits();
             karbonite = gc.karbonite();
 
-            manageUpdates();
+            manageUpgrades();
 
             if( gc.planet() == Planet.Earth ){
                 earth();
@@ -39,6 +31,16 @@ public class Player {
             // Submit the actions we've done, and wait for our next turn.
             gc.nextTurn();
         }
+        */
+        if( gc.planet() == Planet.Earth ){
+            test(gc);
+        }
+        else{
+            System.out.println("\t lol its mars");
+            while(true){
+                gc.nextTurn();
+            }
+        }
     }
 
     /*
@@ -49,7 +51,7 @@ public class Player {
      *
      *  Rocket - Rocketry
      */
-    private static void manageUpgrades(){
+    private static void manageUpgrades(GameController gc){
         gc.researchInfo();
         gc.queueResearch(UnitType.Worker);
     }
@@ -57,14 +59,79 @@ public class Player {
     /*
      * Code for Earth
      */
-    private static void earth(){
+    private static void earth(GameController gc){
         
     }
 
     /*
      * Code for Mars
      */
-    private static void mars(){
+    private static void mars(GameController gc){
         
+    }
+
+    private static Direction getRandom(Directions[] array) {
+        int rnd = new Random().nextInt(array.length);
+        return array[rnd];
+    }
+
+    private static void test(GameController gc){
+        Direction[] directions = Direction.values();
+
+        System.out.println("\nTEST\n");
+
+        Unit r = gc.myUnits().get(0);
+
+        if ( gc.canBlueprint( r.id(), UnitType.Factory, Direction.South ) ){
+            gc.blueprint( r.id(), UnitType.Factory, Direction.South );
+        }
+
+        gc.nextTurn();
+
+        System.out.println("\nCREATED BLUEPRINT\n");
+
+        VecUnit nearby = gc.senseNearbyUnits( r.location().mapLocation() , 2 );
+        Unit factory;
+
+        for( int ai = 0; ai < nearby.size(); ai++ ){
+            System.out.println("\nFOUND BLUEPRINT\n");
+
+            Unit a = nearby.get(ai);
+            if ( a.unitType() == UnitType.Factory ){
+                factory = a;
+                while ( factory.health() != factory.maxHealth() ){
+                    if ( gc.canBuild( r.id(), factory.id() ) ){
+                        gc.build( r.id(), factory.id() );
+                    }
+                    gc.nextTurn();
+                }
+            }
+        }
+
+        while(true){
+
+            for(int u =0; u< gc.myUnits().size(); u++){
+                if( gc.myUnits().get(u).unitType() == UnitType.Factory ){
+                    factory = gc.myUnits().get(u);
+                    if( gc.canProduceRobot( factory.id(), UnitType.Knight ) ){
+                        gc.produceRobot( factory.id(), UnitType.Knight);
+                    }
+                    if( factory.structureGarrison().size() > 0){
+                        Direction dir = getRandom(directions);
+                        if( gc.can_unload(factory.id(), dir) ){
+                            gc.unload(factory.id(), dir);
+                        }
+                    }
+                }
+                if( gc.myUnits().get(u).unitType() == UnitType.Knight ){
+                    VecUnit nearby = gc.senseNearbyUnits( gc.myUnits().get(u).location().mapLocation() , 2 );
+                }
+            }
+            
+        }
+
+        while (true) {
+            gc.nextTurn();
+        }
     }
 }
