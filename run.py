da5d5939a89d06a931aa2f4babc7e4322dfd8641
Andrew Wang <andrewxiaoyu0@gmail.com>
2018-01-13 22:07:27 -0500

    Added pathfinding

--- run.py
+++ run.py
@@ -6,6 +6,10 @@ import traceback
 """
 Help:
 
+UnitData = dict of units and their respective data not included
+    role?
+    destination?
+    
 Root = starting worker where headquarter is
 HQ = headquarter maplocation
 """
@@ -27,7 +31,45 @@ def next_turn():
     sys.stdout.flush()
     sys.stderr.flush()
 def manage_updates():
-    pass
+    updates = [bc.UnitType.Worker, bc.UnitType.Knight, bc.UnitType.Rocket]
+    for u in updates:
+        gc.queue_research(u)
+
+"""
+Pathfinding
+"""
+def find_path(start, end):
+    my_map = gc.starting_map(gc.planet())
+    fringe = [(start, None)]
+
+    for i in range(100): # SET MAX ITERATIONS TO PREVENT LENGTHY SEARCHING
+        loc, path = fringe.pop(0)
+
+        direction = loc.direction_to(end)
+        if direction == bc.Direction.Center:
+            return path
+        new_path = path[:].append(direction)
+        new_loc = loc.add(direction)
+
+        if my_map.on_map(new_loc) and my_map.is_passable_terrain_at(new_loc):
+            fringe.append( (new_loc, new_path) )
+        else:
+            for d in directions:
+                if d != direction:
+                    new_path = path[:].append(d)
+                    new_loc = loc.add(d)
+
+                    if my_map.on_map(new_loc) and my_map.is_passable_terrain_at(new_loc):
+                        fringe.append( (new_loc, new_path) )
+    
+    return False
+
+def move(unit, direction):
+    if gc.is_move_ready(unit.id):
+        if gc.can_move(unit.id, direction):
+            if gc.move_robot(unit.id, direction):
+                return True
+    return False
 
 """
 Unit Management
@@ -51,12 +93,8 @@ def healer(u):
 Planet Control
 """
 def earth():
-    my_map = gc.starting_map(gc.planet())
-    my_coms = gc.get_team_array(gc.planet())
     pass
 def mars():
-    my_map = gc.starting_map(gc.planet())
-    my_coms = gc.get_team_array(gc.planet())
     pass
 
 manage_updates()
